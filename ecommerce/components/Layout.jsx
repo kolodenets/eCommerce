2e942d91846e380ac9e2b6b87bf7987df927153d
Mikhail Kolodenets <mishakolode@gmail.com>
2022-11-28 20:10:38 +0300

    feat: add client and components(Footer,FooterBanner,HeroBanner, Layout, Navbar, Product)

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>
-      Layout
+    <div className='layout'>
+      <Head>
+        <title>
+          Music Gadjects Store
+        </title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   )
 }
